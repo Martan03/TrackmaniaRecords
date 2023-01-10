469d29d10a2f89679fcd6f93203b114bf194bc9a
Martan03 <martinslezak03@gmail.com>
2023-01-10 21:47:07 +0100

    Fixed yellow theme bug

--- model/StatisticsManager.php
+++ model/StatisticsManager.php
@@ -2,6 +2,10 @@
 
 class StatisticsManager
 {
+    /**
+     * Gets record on every map
+     * @return array of records on each map
+     */
     public function getStatistics() : array
     {
         $seasonManager = new SeasonManager();
@@ -36,6 +40,41 @@ class StatisticsManager
         });
         
         return $statistics;
+    }
+
+    /**
+     * Gets all map records in given season
+     * @param array $season to find records in
+     * @return array of records on each map
+     */
+    public function getSeasonStatistics(array $season) : array
+    {
+        $recordManager = new RecordManager();
+        $stats = array();
+
+        $records = $recordManager->getSeasonLevelsRecords($season['season_id']);
+        foreach ($records as $record)
+        {
+            $id = $this->inStatistics('record_holder', $record['record_holder'], $stats);
+            if ($id != -1)
+            {
+                $stats[$id]['record_number']++;
+                continue;
+            }
+
+            if ($record['record_time'] == '')
+                continue;
+
+            array_push($stats, array(
+                'record_holder' => $record['record_holder'],
+                'record_number' => 1
+            ));
+        }
+        usort($stats, function($a, $b) {
+            return $b['record_number'] <=> $a['record_number'];
+        });
+
+        return $stats;
     }
 
     /**
--- presenter/SeasonsPresenter.php
+++ presenter/SeasonsPresenter.php
@@ -6,7 +6,10 @@ class SeasonsPresenter extends Presenter
     {
         $seasonManager = new SeasonManager();
         $recordManager = new RecordManager();
+        $statsManager = new StatisticsManager();
         $this->data['lang'] = getLang();
+
+        // If not parameters, open season overview
         if (empty($params))
         {
             $this->header = array(
@@ -21,12 +24,14 @@ class SeasonsPresenter extends Presenter
             return;
         }
 
+        // Remove record from database
         if (count($params) == 5 && $params[3] == 'remove')
         {
             $recordManager->removeRecord($params[4]);
             $this->redirect('seasons/' . $params[0] . '/' . $params[1] . '/' . $params[2]);
         }
 
+        // Invalid number of parameters, redirect to error page
         if (count($params) < 2 || count($params) > 3)
         {
             $this->redirect("error");
@@ -49,13 +54,16 @@ class SeasonsPresenter extends Presenter
 
         $this->data['season'] = $season;
 
+        // If two parameters, open season details
         if (count($params) == 2)
         {
             $this->data['records'] = $recordManager->getSeasonLevelsRecords($season['season_id']);
+            $this->data['top'] = $statsManager->getSeasonStatistics($season);
             $this->view = 'records';
             return;
         }
 
+        // Last option is level details page
         $this->data['records'] = $recordManager->getRecordsBySeasonLevel($season['season_id'], $params[2]);
         $this->data['level'] = sprintf("%02d", $params[2]);
         $this->data['url'] = $season['season_year'] . '/'

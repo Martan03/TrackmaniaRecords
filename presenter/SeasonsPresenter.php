8e50c2185cec7c223480483e95a192353e6bc3c5
Martan03 <martinslezak03@gmail.com>
2023-01-25 18:11:55 +0100

    Edit SeasonsPresenter

--- presenter/SeasonsPresenter.php
+++ presenter/SeasonsPresenter.php
@@ -4,72 +4,124 @@ class SeasonsPresenter extends Presenter
 {
     public function process(array $params): void
     {
-        $seasonManager = new SeasonManager();
-        $recordManager = new RecordManager();
-        $statsManager = new StatisticsManager();
         $this->data['lang'] = getLang();
 
-        // If not parameters, open season overview
-        if (empty($params))
+        switch (count($params))
         {
-            $this->header = array(
-                'title' => 'Trackmania records',
-                'description' => '',
-                'keywords' => ''
-            );
+            case 0:
+                $this->displaySeasons();
+                break;
+            case 2:
+                $this->displaySeason($params);
+                break;
+            case 3:
+                $this->displayLevel($params);
+                break;
+            case 5:
+                $this->removeRecord($params);
+                break;
+            default:
+                $this->redirect('error');
+                break;
+        }
+    }
     
-            $this->data['seasons'] = $seasonManager->getSeasons();
-            $this->view = 'seasons';
-
-            return;
-        }
-
-        // Remove record from database
-        if (count($params) == 5 && $params[3] == 'remove')
-        {
-            $recordManager->removeRecord($params[4]);
-            $this->redirect('seasons/' . $params[0] . '/' . $params[1] . '/' . $params[2]);
-        }
-
-        // Invalid number of parameters, redirect to error page
-        if (count($params) < 2 || count($params) > 3)
-        {
-            $this->redirect("error");
-            return;
-        }
-
-        $season = $seasonManager->getSeason($params[0], $params[1]);
-
-        if (!$season)
-        {
-            $this->redirect("error");
-            return;
-        }
-
+    /**
+     * Displays all seasons from database
+     */
+    private function displaySeasons() : void
+    {
+        $seasonManager = new SeasonManager();
+        
         $this->header = array(
-            'title' => $season['season_year'] . ' ' . $season['season_name'],
+            'title' => 'Trackmania records',
             'description' => '',
             'keywords' => ''
         );
 
-        $this->data['season'] = $season;
+        $this->data['seasons'] = $seasonManager->getSeasons();
+        $this->view = 'seasons';
+    }
+    
+    /**
+     * Displays season from URL params
+     */
+    private function displaySeason($params) : void
+    {
+        $recMng = new RecordManager();
+        $statMng = new StatisticsManager();
 
-        // If two parameters, open season details
-        if (count($params) == 2)
-        {
-            $this->data['records'] = $recordManager->getSeasonLevelsRecords($season['season_id']);
-            $this->data['top'] = $statsManager->getSeasonStatistics($season);
-            $this->view = 'records';
-            return;
-        }
+        $this->data['season'] = $this->getSeason($params);
+        $this->data['top'] = $statMng->getSeasonStatistics(
+            $this->data['season']
+        );
 
-        // Last option is level details page
-        $this->data['records'] = $recordManager->getRecordsBySeasonLevel($season['season_id'], $params[2]);
+        $this->header = array(
+            'title' => $this->data['season']['season_year'] . ' ' 
+                     . $this->data['season']['season_name'],
+            'description' => '',
+            'keywords' => ''
+        );
+        
+        $this->data['records'] = $recMng->getSeasonLevelsRecords(
+            $this->data['season']['season_id']
+        );
+        
+        $this->view = 'records';
+    }
+
+    /**
+     * Displays level from URL params from database
+     */
+    private function displayLevel($params) : void
+    {
+        $recMng = new RecordManager();
+
+        $this->data['season'] = $this->getSeason($params);
         $this->data['level'] = sprintf("%02d", $params[2]);
-        $this->data['url'] = $season['season_year'] . '/'
-                            . $season['season_name'] . '/'
+
+        $this->header = array(
+            'title' => $this->data['season']['season_year'] . ' ' 
+                     . $this->data['season']['season_name'] . ' - '
+                     . $this->data['level'],
+            'description' => '',
+            'keywords' => ''
+        );
+
+        $this->data['records'] = $recMng->getRecordsBySeasonLevel(
+            $this->data['season']['season_id'],
+            $params[2]
+        );
+        $this->data['url'] = $this->data['season']['season_year'] . '/'
+                            . $this->data['season']['season_name'] . '/'
                             . $params[2];
-        $this->header['title'] .= ' - ' . $this->data['level'];
+
         $this->view = 'level';
+    }
+
+    /**
+     * Removes record from database
+     */
+    private function removeRecord($params) : void
+    {
+        if ($params[3] != 'remove')
+            $this->redirect('error');
+        
+        $recMng = new RecordManager();
+        $recMng->removeRecord($params[4]);
+        $this->redirect('seasons/' . $params[0] . '/' . $params[1] . '/' . $params[2]);
+    }
+
+    /**
+     * Gets season from database, if not found redirects to error page
+     */
+    private function getSeason($params) : array
+    {
+        $seasonManager = new SeasonManager();
+        $season = $seasonManager->getSeason($params[0], $params[1]);
+
+        if (!$season)
+            $this->redirect('error');
+        return $season;
     }
 }
